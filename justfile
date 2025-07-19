cd257fc5745b9aa28db2f4da971eb1768fd6bef9
Hunter Matthews <hunter@unix.haus>
2025-07-19 17:52:20 -0400

    parameterize tests and add a justfile test target

--- justfile
+++ justfile
@@ -4,6 +4,7 @@
 _default: help
 
 # Clean up build artifacts
+[group('build')]
 clean:
 	rm -rf build dist
 	find src -type d -name '*.egg-info' -exec rm -r {} + -depth
@@ -13,7 +14,13 @@ clean:
 help:
     @just --list
 
+# Run tests using pytest
+[group('test')]
+test *args:
+    uv run pytest tests {{args}}
+
 # zip the program as a single file executable (zipapp)
+[group('build')]
 zip:
     mkdir -p dist
     uv run python -m zipapp src -m "clu.cli:main" -o dist/clu -p "/usr/bin/env python3"
--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -4,32 +4,39 @@ import pytest
 from clu.conversions import bytes_to_si, si_to_bytes
 
 
-def test_bytes_to_si():
-    assert bytes_to_si(1536) == "1.5 KB"
-    assert bytes_to_si(1048576) == "1.0 MB"
-    assert bytes_to_si(1073741824) == "1.0 GB"
-    assert bytes_to_si(1099511627776) == "1.0 TB"
-    assert bytes_to_si(1125899906842624) == "1.0 PB"
-    assert bytes_to_si(1152921504606846976) == "1.0 EB"
-    assert bytes_to_si(0) == "0.0 B"
-    assert bytes_to_si(1023) == "1023.0 B"
+@pytest.mark.parametrize(
+    "si_string, expected_output",
+    [
+        ("1.5 KB", 1536),
+        ("1.0 MB", 1048576),
+        ("1.0 GB", 1073741824),
+        ("1.0 TB", 1099511627776),
+        ("1.0 PB", 1125899906842624),
+        ("1.0 EB", 1152921504606846976),
+        ("0.0 B", 0),
+        ("0 B", 0),
+        ("1023 B", 1023),
+    ]
+)
+def test_si_to_bytes(si_string, expected_output):
+    bytes_count = si_to_bytes(si_string)
 
+    assert bytes_count == expected_output
 
-def test_si_to_bytes():
-    assert si_to_bytes("0.0 B") == 0
-    assert si_to_bytes("0 B") == 0
-    assert si_to_bytes("1023 B") == 1023
+@pytest.mark.parametrize(
+    "bytes_count, expected_output",
+    [
+        (1536, "1.5 KB"),
+        (1048576, "1.0 MB"),
+        (1073741824, "1.0 GB"),
+        (1099511627776, "1.0 TB"),
+        (1125899906842624, "1.0 PB"),
+        (1152921504606846976, "1.0 EB"),
+        (0, "0.0 B"),
+        (1023, "1023.0 B"),
+    ]
+)
+def test_bytes_to_si(bytes_count, expected_output):
+    si_string = bytes_to_si(bytes_count)
 
-    assert si_to_bytes("1.5 KB") == 1536
-    assert si_to_bytes("1.0 MB") == 1048576
-    assert si_to_bytes("1.0 GB") == 1073741824
-    assert si_to_bytes("1.0 TB") == 1099511627776
-    assert si_to_bytes("1.0 PB") == 1125899906842624
-    assert si_to_bytes("1.0 EB") == 1152921504606846976
-
-    # assert si_to_bytes("1 KB") == 1000
-    # assert si_to_bytes("1 MB") == 1000000
-    # assert si_to_bytes("1 GB") == 1000000000
-    # assert si_to_bytes("1 TB") == 1000000000000
-    # assert si_to_bytes("1 PB") == 1000000000000000
-    # assert si_to_bytes("1 EB") == 1000000000000000000
+    assert si_string == expected_output
