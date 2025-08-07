86d89a86ce72af2198dc729d353ac911128bd7a3
Hunter Matthews <hunter.matthews@nih.gov>
2025-08-07 19:36:23 -0400

    Implement parse_ipmitool
    Both "mc info" and "lan print" get parsed for basics.

--- src/clu/os_linux.py
+++ src/clu/os_linux.py
@@ -48,6 +48,7 @@ def provides_os_linux() -> Provides:
     provides_no_salt(provides)
     provides_proc_uptime(provides)
     provides_clu(provides)
+    provides_ipmitool(provides)
 
     return provides
 
@@ -67,6 +68,7 @@ def requires_os_linux() -> Requires:
     requires_no_salt(requires)
     requires_proc_uptime(requires)
     requires_clu(requires)
+    requires_ipmitool(requires)
 
     return requires
 
@@ -88,6 +90,7 @@ def parse_os_linux() -> Facts:
     parse_no_salt(facts)
     parse_proc_uptime(facts)
     parse_clu(facts)
+    parse_ipmitool(facts)
 
     return facts
 
@@ -419,5 +422,75 @@ def parse_ip_addr(facts: Facts) -> None:
     log.debug(f"{data=}")
 
 
-# TODO:
-# 1. Create parse_ram() function to parse ls_mem and if that fails fallback to udevadm_ram()
+def provides_ipmitool(provides: Provides) -> None:
+    for key in [
+        "bmc.ipv4_source",
+        "bmc.ipv4_address",
+        "bmc.ipv4_mask",
+        "bmc.mac_address",
+        "bmc.firmware_version",
+        "bmc.manufacturer_id",
+        "bmc.manufacturer_name",
+    ]:
+        provides[key] = parse_ipmitool
+
+
+def requires_ipmitool(requires: Requires) -> None:
+    requires.programs.append("ipmitool")
+
+
+def _parse_ipmitool_lan_print(facts: Facts) -> None:
+    regexes = {
+        r"^IP Address Source *: (.+)": "bmc.ipv4_source",
+        r"^IP Address *: (.+)": "bmc.ipv4_address",
+        r"^Subnet Mask *: (.+)": "bmc.ipv4_mask",
+        r"^MAC Address *: (.+)": "bmc.mac_address",
+    }
+    fields = {}
+    data, rc = read_program("ipmitool lan print")
+    if data is None or rc != 0:
+        return
+
+    for regex, field in regexes.items():
+        match = re.search(regex, data, re.MULTILINE)
+        value = match.group(1).strip() if match else None
+        log.debug(f"{value=}")
+        if value is not None:
+            fields[field] = value
+    log.debug(f"{fields=}")
+
+    facts.update(fields)
+
+
+def _parse_ipmitool_mc_info(facts: Facts) -> None:
+    regexes = {
+        r"^Firmware Revision *: (.+)": "bmc.firmware_version",
+        r"^Manufacturer ID *: (.+)": "bmc.manufacturer_id",
+        r"^Manufacturer Name *: (.+)": "bmc.manufacturer_name",
+    }
+    fields = {}
+    data, rc = read_program("ipmitool mc info")
+    if data is None or rc != 0:
+        return
+
+    for regex, field in regexes.items():
+        match = re.search(regex, data, re.MULTILINE)
+        value = match.group(1).strip() if match else None
+        log.debug(f"{value=}")
+        if value is not None:
+            fields[field] = value
+    log.debug(f"{fields=}")
+
+    facts.update(fields)
+
+
+def parse_ipmitool(facts: Facts) -> None:
+    if "phy.platform" not in facts:
+        parse_virt_what(facts)
+
+    if facts["phy.platform"] != "physical":
+        log.info("Not a physical platform, skipping bmc. parsing")
+        return
+
+    _parse_ipmitool_mc_info(facts)
+    _parse_ipmitool_lan_print(facts)
---
+++ tests/test_parse_ipmitool.py
@@ -0,0 +1,57 @@
+import pytest
+from unittest.mock import patch
+
+from clu import config
+from clu.facts import Facts
+from clu.os_linux import parse_ipmitool
+
+from tests import mock_read_program
+
+
+@pytest.mark.parametrize(
+    "mock_host, expected_result",
+    [
+        (
+            "host1",
+            {
+                "phy.platform": "vmware",
+            },
+        ),
+        (
+            "host2",
+            {
+                "phy.platform": "vmware",
+            },
+        ),
+        (
+            "host3",
+            {
+                "phy.platform": "physical",
+                "bmc.ipv4_source": "DHCP Address",
+                "bmc.ipv4_address": "10.136.28.96",
+                "bmc.ipv4_mask": "255.255.255.0",
+                "bmc.mac_address": "d0:46:0c:70:51:b8",
+                "bmc.firmware_version": "7.10",
+                "bmc.manufacturer_id": "674",
+                "bmc.manufacturer_name": "DELL Inc",
+            },
+        ),
+        (
+            "macos",
+            {
+                "phy.platform": "Unknown/Error",
+            },
+        ),
+    ],
+)
+def test_parse_ipmitool(mock_host, expected_result):
+    """Test parse_ipmitool function with mock data from different hosts."""
+
+    with patch("clu.os_linux.read_program") as mrf:
+        mrf.side_effect = lambda cmdline: mock_read_program(pytest.mock_dir / mock_host, cmdline)
+
+        facts = Facts()
+        parse_ipmitool(facts)
+
+        # Assert the expected results
+        assert facts == expected_result, mock_host
