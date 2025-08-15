0c7bdfde284cb40273d148a7be8a7723250d858c
Hunter Matthews <hunter.matthews@nih.gov>
2025-08-14 20:21:55 -0400

    Convert operating systems from 4-5 related functions to a class
    ported from a shell script, so we intro OOP now.

---
+++ src/clu/darwin.py
@@ -0,0 +1,30 @@
+"""Doc Incomplete."""
+
+import logging
+
+from clu import Provides
+from clu import Requires
+from clu import Facts
+from clu.opsys import OpSys
+
+from clu.sources import uname, sw_vers, macos_name, uptime, clu
+
+log = logging.getLogger(__name__)
+
+
+class Darwin(OpSys):
+    """macOS (Darwin) operating system class."""
+
+    _sources = [uname.Uname, sw_vers.SwVers, macos_name.MacOSName, uptime.Uptime, clu.Clu]
+
+    def default_facts(self) -> list:
+        return ["os.name", "os.hostname", "os.version", "os.codename", "run.uptime", "clu.version"]
+
+    def parse(self) -> Facts:
+        """Parse the facts for macOS (Darwin)."""
+        facts = super().parse()
+
+        # Nothing explicitly says Apple, but we know its apple because Darwin is the OS
+        facts["sys.vendor"] = "Apple"
+
+        return facts
