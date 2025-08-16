7731641a0e235c50818f1b0cdcd9543f279164fa
Hunter Matthews <hunter@unix.haus>
2025-08-16 15:54:56 -0400

    redo the report runner

--- src/clu/cmd/report.py
+++ src/clu/cmd/report.py
@@ -1,8 +1,10 @@
+import argparse
 import json
 import logging
 
 from clu.opsys.factory import opsys_factory
 from clu import Facts
+from clu.opsys import OpSys
 
 
 log = logging.getLogger(__name__)
@@ -27,53 +29,74 @@ def parse_args(subparsers):
     subp_report.add_argument("facts", nargs="*", help="Facts to report on")
 
 
+def set_report_defaults(opsys: OpSys, args: argparse.Namespace) -> None:
+    """This is written defensively because if the user didn't say "report" explicitly on the command
+    line, we want to make sure we still have a valid set of facts to report on.
+    """
+    if "output" not in args:
+        args.output = "dots"
+
+    if "all" in args and args.all:
+        # BUG: this does not include the bmc facts..
+        args.facts = "os sys phy run salt clu"
+    elif "facts" not in args:
+        args.facts = opsys.default_facts()
+
+
+def parse_facts_from_specs(provides_map, parsed_facts: Facts, fact_specs) -> None:
+    sources_to_parse = set()
+
+    # Loop through the facts that were requested on the command line and get a set of parsers that
+    # will obtain those facts (there's likely duplicates, so we use a set here)
+    for fact_spec in fact_specs:
+        for key in provides_map:
+            if key.startswith(fact_spec):
+                sources_to_parse.add(provides_map[key])
+
+    # Call the parsers that we found in the previous loop
+    for source in sources_to_parse:
+        log.debug(f"Calling parser function {source}")
+        source.parse(parsed_facts)
+
+
+def filter_facts(requested_fact_specs, parsed_facts: Facts) -> Facts:
+    """Filter the parsed facts based on the requested fact specifications."""
+
+    # Loop through the facts that were requested on the command line (again) and make a new
+    # facts list/dict that has JUST those matching facts
+    output_facts = Facts()
+    for fact_spec in requested_fact_specs:
+        for key in parsed_facts:
+            if key.startswith(fact_spec):
+                output_facts[key] = parsed_facts[key]
+    return output_facts
+
+
+def do_output(output_facts: Facts, output_arg: str) -> None:
+    if output_arg == "json":
+        output_json(output_facts)
+    elif output_arg == "shell":
+        output_shell(output_facts)
+    elif output_arg == "dots":
+        output_dots(output_facts)
+
+
 def report_facts(args) -> int:
     """Generate a report based on the current OS."""
 
     log.debug(f"Running command {args.cmd} with args={args}")
 
     opsys = opsys_factory()
-
     provides_map = opsys.provides()
-    sources_to_parse = set()
-
-    # This is written defensively because if the user didn't say "report" explicitly on the command
-    # line, we want to make sure we still have a valid set of facts to report on
-    if "all" in args and args.all:
-        args.facts = "os sys phy run salt clu"
-    elif "facts" not in args:
-        args.facts = opsys.default_facts()
-
-    # Loop through the facts that were requested on the command line and get a set of parsers that
-    # will obtain those facts (there's likely duplicates, so we use a set here)
-    for fact_spec in args.facts:
-        for key in provides_map:
-            if key.startswith(fact_spec):
-                sources_to_parse.add(provides_map[key])
-
-    # Call the parsers that we found in the previous loop
     parsed_facts = Facts()
-    for source in sources_to_parse:
-        log.debug(f"Calling parser function {source}")
-        source.parse(parsed_facts)
 
-    # Loop through the facts that were requested on the command line (again) and make a new
-    # facts list/dict that has JUST those matching facts
-    output_facts = Facts()
-    for fact_spec in args.facts:
-        for key in parsed_facts:
-            if key.startswith(fact_spec):
-                output_facts[key] = parsed_facts[key]
+    set_report_defaults(opsys, args)
 
-    if "output" not in args:
-        args.output = "dots"
+    parse_facts_from_specs(provides_map, parsed_facts, args.facts)
 
-    if args.output == "json":
-        output_json(output_facts)
-    elif args.output == "shell":
-        output_shell(output_facts)
-    elif args.output == "dots":
-        output_dots(output_facts)
+    output_facts = filter_facts(args.facts, parsed_facts)
+
+    do_output(output_facts, args.output)
 
     return 0
 
