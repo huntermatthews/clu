3c95ef96acd8fcc046e99e10e5c6e374ba2d6cc6
Hunter Matthews <hunter.matthews@nih.gov>
2025-08-18 19:24:29 -0400

    Re-implement test_ipmitool with new json based fixture.

--- tests/sources/test_ipmitool.py
+++ tests/sources/test_ipmitool.py
@@ -4,65 +4,42 @@ from unittest.mock import patch
 from clu import Facts
 from clu.sources.ipmitool import Ipmitool
 
-from tests import mock_read_program
+from tests import dict_subset, mock_read_program, mock_data_dir
+
+
+input_keys = ["phy.platform"]
+output_keys = [
+    "phy.platform",
+    "bmc.ipv4_source",
+    "bmc.ipv4_address",
+    "bmc.ipv4_mask",
+    "bmc.mac_address",
+    "bmc.firmware_version",
+    "bmc.manufacturer_id",
+    "bmc.manufacturer_name",
+]
 
 
 @pytest.mark.parametrize(
-    "mock_host, input_facts, expected_result",
+    "mock_host, input_keys, output_keys",
     [
-        (
-            "host1",
-            {
-                "phy.platform": "vmware",
-            },
-            {
-                "phy.platform": "vmware",
-            },
-        ),
-        (
-            "host2",
-            {
-                "phy.platform": "vmware",
-            },
-            {
-                "phy.platform": "vmware",
-            },
-        ),
-        (
-            "host3",
-            {
-                "phy.platform": "physical",
-            },
-            {
-                "phy.platform": "physical",
-                "bmc.ipv4_source": "DHCP Address",
-                "bmc.ipv4_address": "10.136.28.96",
-                "bmc.ipv4_mask": "255.255.255.0",
-                "bmc.mac_address": "d0:46:0c:70:51:b8",
-                "bmc.firmware_version": "7.10",
-                "bmc.manufacturer_id": "674",
-                "bmc.manufacturer_name": "DELL Inc",
-            },
-        ),
-        (
-            "macos",
-            {
-                "phy.platform": "Unknown/Error",
-            },
-            {
-                "phy.platform": "Unknown/Error",
-            },
-        ),
+        ("host1", input_keys, output_keys),
+        ("host2", input_keys, output_keys),
+        ("host3", input_keys, output_keys),
     ],
 )
-def test_ipmitool_parse(mock_host, input_facts, expected_result):
+def test_ipmitool_parse(mock_host, input_keys, output_keys, host_json_loader):
+    host_all_facts = host_json_loader(mock_host)
+    host_input_facts = dict_subset(host_all_facts, input_keys)
+    host_output_facts = dict_subset(host_all_facts, output_keys)
+
     with patch("clu.sources.ipmitool.text_program") as mrf:
-        mrf.side_effect = lambda cmdline: mock_read_program(pytest.mock_dir / mock_host, cmdline)
+        mrf.side_effect = lambda cmdline: mock_read_program(mock_data_dir / mock_host, cmdline)
 
         facts = Facts()
-        facts.update(input_facts)
+        facts.update(host_input_facts)
         ipmitool = Ipmitool()
         ipmitool.parse(facts)
 
         # Assert the expected results
-        assert facts == expected_result, mock_host
+        assert facts == host_output_facts, mock_host
