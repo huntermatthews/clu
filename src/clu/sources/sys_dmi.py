f9c522e83daf04ef5e2d64953b648b1ed4b8686f
Hunter Matthews <hunter@unix.haus>
2025-08-30 01:14:41 -0400

    Rewrite sys_dmi to use a dict and be considerably simplified.

--- src/clu/sources/sys_dmi.py
+++ src/clu/sources/sys_dmi.py
@@ -8,59 +8,30 @@ log = logging.getLogger(__name__)
 
 
 class SysDmi(Source):
+    _dmap = {
+        "sys.vendor": "/sys/devices/virtual/dmi/id/sys_vendor",
+        "sys.model.family": "/sys/devices/virtual/dmi/id/product_family",
+        "sys.model.name": "/sys/devices/virtual/dmi/id/product_name",
+        "sys.serial_no": "/sys/devices/virtual/dmi/id/product_serial",
+        "sys.uuid": "/sys/devices/virtual/dmi/id/product_uuid",
+        "sys.oem": "/sys/devices/virtual/dmi/id/chassis_vendor",
+        "sys.asset_no": "/sys/devices/virtual/dmi/id/chassis_asset_tag",
+    }
+
     def provides(self, provides: Provides) -> None:
-        provides["sys.vendor"] = self
-        provides["sys.model.family"] = self
-        provides["sys.model.name"] = self
-        provides["sys.serial_no"] = self
-        provides["sys.uuid"] = self
-        provides["sys.oem"] = self
-        provides["sys.asset_no"] = self
+        for key in self._dmap.keys():
+            provides[key] = self
 
     def requires(self, requires: Requires) -> None:
-        requires.files.extend(
-            [
-                "/sys/devices/virtual/dmi/id/sys_vendor",
-                "/sys/devices/virtual/dmi/id/product_family",
-                "/sys/devices/virtual/dmi/id/product_name",
-                "/sys/devices/virtual/dmi/id/product_serial",
-                "/sys/devices/virtual/dmi/id/product_uuid",
-                "/sys/devices/virtual/dmi/id/chassis_vendor",
-                "/sys/devices/virtual/dmi/id/chassis_asset_tag",
-            ]
-        )
+        requires.files.append(self._dmap.values())
 
     def parse(self, facts: Facts) -> None:
         if facts["phy.platform"] != "physical":
             log.info("Not a physical platform, skipping sys.dmi parsing")
             return
 
-        keys = [
-            "sys.vendor",
-            "sys.model.family",
-            "sys.model.name",
-            "sys.serial_no",
-            "sys.uuid",
-            "sys.oem",
-            "sys.asset_no",
-        ]
-        entries = [
-            "sys_vendor",
-            "product_family",
-            "product_name",
-            "product_serial",
-            "product_uuid",
-            "chassis_vendor",
-            "chassis_asset_tag",
-        ]
-        if len(keys) != len(entries):
-            log.debug(f"{keys=}")
-            log.debug(f"{entries=}")
-            panic("parse_sys_dmi: keys and entries length don't match: You can't count")
-        for idx in range(len(keys)):
-            key = keys[idx]
-            entry = entries[idx]
-            data = text_file(f"/sys/devices/virtual/dmi/id/{entry}")
+        for key, entry in self._dmap.items():
+            data = text_file(entry)
             log.debug(f"{data=}")
             log.debug(f"{key=}")
             facts[key] = data.strip() if data else ""
