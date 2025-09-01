52fac7ebcc405e4528b98da8084d4f5f6e86533d
Hunter Matthews <hunter.matthews@nih.gov>
2025-09-01 15:33:39 -0400

    Implement lsmem as the primary way to check ram size.
    replace udevadm info, as that proved to generate HUGE output and to not always include the required information on older systems.
    Luckily we got lsmem installed across the fleet consistently -
    I hate linux has only gotten lsmem "recently" should have been there in the 90's.

---
+++ tests/sources/test_lsmem.py
@@ -0,0 +1,31 @@
+import pytest
+from unittest.mock import patch
+
+from clu import Facts
+from clu.sources.lsmem import Lsmem
+from clu.sources import PARSE_FAIL_MSG
+
+from tests import mock_read_program, mock_data_dir
+
+
+@pytest.mark.parametrize(
+    "mock_host, expected_result",
+    [
+        ("host1", {"phy.ram": "4.0 GB"}),
+        ("host2", {"phy.ram": "4.0 GB"}),
+        ("host3", {"phy.ram": "64.0 GB"}),
+        ("macos", {"phy.ram": PARSE_FAIL_MSG}),
+    ],
+)
+def test_lsmem_parse(mock_host, expected_result):
+    """Test parse_lsmem function with mock data from different hosts."""
+
+    with patch("clu.sources.lsmem.text_program") as mrf:
+        mrf.side_effect = lambda cmdline: mock_read_program(mock_data_dir / mock_host, cmdline)
+
+        facts = Facts()
+        lsmem = Lsmem()
+        lsmem.parse(facts)
+
+        # Assert the expected results
+        assert facts == expected_result, mock_host
