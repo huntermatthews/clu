86e8ef45d48db64988d582808bbabca9ab2af5d2
Hunter Matthews <hunter.matthews@nih.gov>
2025-09-05 18:21:05 -0400

    First rough cut of a Device42 source.
    Lots to do here.

--- notes/d42/d42_query.py
+++ notes/d42/d42_query.py
@@ -1,19 +1,18 @@
-
 import urllib.request
 import base64
 import json
 
 from creds import D42_URL, D42_USERNAME, D42_PASSWORD
 
-ORIGINAL_DEVICE_NAME = 'blueplate'       #original device name
+ORIGINAL_DEVICE_NAME = "blueplate"  # original device name
 
 
-d42_get_devices_url = D42_URL+'/api/1.0/devices/all/'
-d42_get_device_url = D42_URL+'/api/1.0/device/name/' + ORIGINAL_DEVICE_NAME #+ '/'
+d42_get_devices_url = D42_URL + "/api/1.0/devices/all/"
+d42_get_device_url = D42_URL + "/api/1.0/device/name/" + ORIGINAL_DEVICE_NAME  # + '/'
 request = urllib.request.Request(d42_get_device_url)
-credentials = f'{D42_USERNAME}:{D42_PASSWORD}'
-encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('ascii')
-request.add_header('Authorization', f'Basic {encoded_credentials}')
+credentials = f"{D42_USERNAME}:{D42_PASSWORD}"
+encoded_credentials = base64.b64encode(credentials.encode("utf-8")).decode("ascii")
+request.add_header("Authorization", f"Basic {encoded_credentials}")
 
 r = urllib.request.urlopen(request)
 if r.getcode() == 200:
@@ -28,25 +27,25 @@ else:
 
 #
 
-    # data = urllib.urlencode(params)
-    # headers = {
-    #     'Authorization': 'Basic ' + base64.b64encode(D42_USERNAME + ':' + D42_PASSWORD),
-    #     'Content-Type': 'application/x-www-form-urlencoded'}
-    # req = urllib2.Request(D42_API_URL, data, headers)
+# data = urllib.urlencode(params)
+# headers = {
+#     'Authorization': 'Basic ' + base64.b64encode(D42_USERNAME + ':' + D42_PASSWORD),
+#     'Content-Type': 'application/x-www-form-urlencoded'}
+# req = urllib2.Request(D42_API_URL, data, headers)
 
-    # def uploader(self, data, url):
-    #         payload = data
-    #         headers = {
-    #             'Authorization': 'Basic ' + base64.b64encode(self.username + ':' + self.password),
-    #             'Content-Type': 'application/json'
-    #         }
+# def uploader(self, data, url):
+#         payload = data
+#         headers = {
+#             'Authorization': 'Basic ' + base64.b64encode(self.username + ':' + self.password),
+#             'Content-Type': 'application/json'
+#         }
 
-    #         r = requests.post(url, data=json.dumps(payload), headers=headers, verify=False)
-    #         msg =  unicode(payload)
-    #         if self.debug:
-    #             print msg
-    #         msg = 'Status code: %s' % str(r.status_code)
-    #         print msg
-    #         msg = str(r.text)
-    #         if self.debug:
-    #             print msg
+#         r = requests.post(url, data=json.dumps(payload), headers=headers, verify=False)
+#         msg =  unicode(payload)
+#         if self.debug:
+#             print msg
+#         msg = 'Status code: %s' % str(r.status_code)
+#         print msg
+#         msg = str(r.text)
+#         if self.debug:
+#             print msg
