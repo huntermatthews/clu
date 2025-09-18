1778b42a041312f8c94c94dad3efe2eb6639c66a
Hunter Matthews <hunter@unix.haus>
2025-09-18 17:33:31 -0400

    Finish implementing tiers (of facts)
    Only source clu and system_version_plist for now but its a start.
    All to make d42 and imds output sensible... sheesh.

--- src/clu/facts.py
+++ src/clu/facts.py
@@ -3,19 +3,42 @@ from typing import Any
 
 
 class Tier(Enum):
-    PRIMARY = "primary"
-    SECONDARY = "secondary"
-    OTHER = "other"
+    ONE = "one"
+    TWO = "two"
+    THREE = "three"
+
+    def get_by_int(index: int) -> "Tier":
+        if index == 1:
+            return Tier.ONE
+        elif index == 2:
+            return Tier.TWO
+        elif index == 3:
+            return Tier.THREE
+        else:
+            raise ValueError("Invalid tier index")
 
 
 class Facts:
     def __init__(self):
-        self._priority = {Tier.PRIMARY: [], Tier.SECONDARY: [], Tier.OTHER: []}
+        self._tier = {Tier.ONE: [], Tier.TWO: [], Tier.THREE: []}
         self._facts = {}
 
     def add(self, priority: Tier, key: str, value: str) -> None:
-        self._priority[priority].append(key)
+        self._tier[priority].append(key)
         self._facts[key] = value
+
+    def get_tier(self, tier: Tier) -> list:
+        result = []
+        if tier == Tier.ONE:
+            result = self._tier[Tier.ONE]
+        elif tier == Tier.TWO:
+            result = self._tier[Tier.ONE] + self._tier[Tier.TWO]
+        elif tier == Tier.THREE:
+            result = self._tier[Tier.ONE] + self._tier[Tier.TWO] + self._tier[Tier.THREE]
+        return result
+
+    def to_dict(self) -> dict:
+        return self._facts
 
     def update(self, other: dict):
         for key in other:
@@ -25,18 +48,19 @@ class Facts:
         return item in self._facts
 
     def __eq__(self, other) -> bool:
-        if not isinstance(other, Facts):
-            return NotImplemented
-
-        if self._facts != other._facts:
-            return False
-        # FIXME: when the tests can handle this, reactivate
-        # if self._priority != other._priority:
-        #     print("self:", self._priority)
-        #     print("other:", other._priority)
-        #     print("xx: _priority not equal")
-        #     return False
-        return True
+        if isinstance(other, Facts):
+            if self._facts != other._facts:
+                return False
+            # FIXME: when the tests can handle this, reactivate
+            # if self._tier != other._tier:
+            #     print("self:", self._tier)
+            #     print("other:", other._tier)
+            #     print("xx: _tier not equal")
+            #     return False
+            return True
+        if isinstance(other, dict):
+            return self._facts == other
+        return False
 
     def __getitem__(self, key: str) -> Any:
         return self._facts.get(key)
@@ -46,11 +70,11 @@ class Facts:
             yield item
 
     def __repr__(self) -> str:
-        return f"Facts: {self._facts}, {self._priority}"
+        return f"Facts: {self._facts}, {self._tier}"
 
     def __setitem__(self, key: str, value: Any) -> None:
         self._facts[key] = value
-        self._priority[Tier.PRIMARY].append(key)
+        self._tier[Tier.ONE].append(key)
 
     def __str__(self) -> str:
-        return f"Facts: {self._facts}, {self._priority}"
+        return f"Facts: {self._facts}, {self._tier}"
